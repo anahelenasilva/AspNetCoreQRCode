07e3c172aff9339ffc9c7c9f5fbeb0940958f8d1
Ana Helena <anahelenarp@hotmail.com>
2021-03-17 17:14:24 -0300

    Adding code

---
+++ QrWeb/Pages/QrCode.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QrWeb.Pages
+{
+    public class QrCodeModel : PageModel
+    {
+        public IActionResult OnGet([FromQuery] string url)
+        {
+            var image = QrCodeGenerator.GenerateByteArray(url);
+            return File(image, "image/png");
+        }
+    }
+}
